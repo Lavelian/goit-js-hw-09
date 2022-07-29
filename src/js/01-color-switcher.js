db96890c4d47c653759da0b1e80e22cafa05a435
Lavelian <daniil.solodkyi@gmail.com>
2022-07-29 18:05:46 +0300

    final task 3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,8 +1,11 @@
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
 const bodyEl = document.querySelector('body');
+
+
 let timerId = null;
 btnStop.disabled = true;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const dataObj = {};
+
+const formEl = document.querySelector('.form');
+
+function onSubmitForm(evt) {
+  evt.preventDefault();
+  evt.target.reset();
+  console.log(dataObj);
+
+  for (let i = 1; i <= dataObj.amount; i++) {
+    createPromise(i, (dataObj.delay += dataObj.step))
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onInput(evt) {
+  dataObj[evt.target.name] = Number(evt.target.value);
+}
+
+formEl.addEventListener('submit', onSubmitForm);
+formEl.addEventListener('input', onInput);
