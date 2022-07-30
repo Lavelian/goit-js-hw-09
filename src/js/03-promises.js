e47b25754f8b4da967fdd53d5ac0bfd33433f739
Lavelian <daniil.solodkyi@gmail.com>
2022-07-30 12:06:44 +0300

    task_3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -24,6 +24,16 @@ function createPromise(position, delay) {
 function onSubmitForm(evt) {
   evt.preventDefault();
   for (let i = 1; i <= dataObj.amount; i++) {
+    if (i === 1) {
+      createPromise(i, dataObj.delay)
+        .then(({ position, delay }) => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+      continue;
+    }
     createPromise(i, (dataObj.delay += dataObj.step))
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
